61e604fe0f005657737bcea202ea93098d174ba9
aaditya-shrivastava <aadityashrivastava2004@gmail.com>
2024-07-24 02:52:38 +0530

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,20 @@
+let hour = document.getElementById("hours");
+let minute = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+function Timer(){
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hRotation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+    
+    hour.style.transform = `rotate(${hRotation}deg)`;
+    minute.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+
+}
+setInterval(Timer,100);
